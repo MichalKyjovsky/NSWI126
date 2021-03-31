d52fa1de3cd63232e527d2a58b6aaa7e627bdfab
Michal Kyjovsky <michal.kyjovsky@getmanta.com>
2021-03-31 08:12:59 +0200

    Short presentation deploy

--- airflow/dags/dag_1.py
+++ airflow/dags/dag_1.py
@@ -1,1 +1,29 @@
 # TODO: Implement me - example DAG
+# step - 1
+
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.dummy_operator import DummyOperator
+
+# step - 2
+
+default_args = {
+    'owner': 'airflow',
+    'depedends_on_past': False,
+    'start_date': datetime(2021, 3, 28),
+    'retries': 0
+}
+
+# step - 3
+
+dag = DAG(dag_id="DAG-1", default_args=default_args, catchup=False, schedule_interval='@once')
+
+# step - 4
+
+start = DummyOperator(task_id='start', dag=dag)
+end = DummyOperator(task_id='end', dag=dag)
+
+# step - 5
+
+start >> end
+
